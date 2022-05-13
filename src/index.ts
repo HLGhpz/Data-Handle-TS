17cf139e0917083389dd9986a8c62019681051f4
hlghpz <1090377416@qq.com>
2022-05-13 21:51:17 +0800

    feat(utils): 新增 Array2Json 工具。

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: HLGhpz
  * @Date: 2022-04-17 14:50:55
  * @LastEditors: HLGhpz
- * @LastEditTime: 2022-05-12 11:24:54
+ * @LastEditTime: 2022-05-13 19:05:04
  * @Description:
  *
  * Copyright (c) 2022 by HLGhpz, All Rights Reserved.
@@ -10,6 +10,8 @@
 
 import FillNationCode from './utils/FillNationCode'
 import BulkInsertDB from './utils/BulkInsertDB'
+import Array2Json from './utils/Array2Json'
 
 // BulkInsertDB()
-FillNationCode()
+// FillNationCode()
+Array2Json()
