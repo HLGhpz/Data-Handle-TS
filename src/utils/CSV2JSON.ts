5d5e14be90d13dabab84350df7bf770bb8412879
HLGhpz <1090377416@qq.com>
2022-06-01 17:33:43 +0800

    test(utils): 数据更新。

--- src/utils/CSV2JSON.ts
+++ src/utils/CSV2JSON.ts
@@ -2,7 +2,7 @@
  * @Author: HLGhpz
  * @Date: 2022-05-12 23:45:48
  * @LastEditors: HLGhpz
- * @LastEditTime: 2022-05-26 19:05:19
+ * @LastEditTime: 2022-06-01 11:39:30
  * @Description:
  *
  * Copyright (c) 2022 by HLGhpz, All Rights Reserved.
@@ -11,13 +11,15 @@
 import fs from 'fs'
 import path from 'path'
 import DataSet from '@antv/data-set'
+import _ from 'lodash'
 
+const __fileName = 'GaoKao'
 const __dirname = path.resolve()
 const IMPORT_FILE_PATH = path.join(
   __dirname,
-  './src/rowData/ProvincePopulation.csv'
+  `./src/rowData/Other/${__fileName}.csv`
 )
-const EXPORT_FILE_PATH = path.join(__dirname, './distData/temp.json')
+const EXPORT_FILE_PATH = path.join(__dirname, `./distData/${__fileName}.json`)
 
 function CSV2JSON() {
   const dv = new DataSet.View().source(
@@ -25,29 +27,19 @@ function CSV2JSON() {
     {
       type: 'csv'
     }
-  )
-  dv.transform({
-    type: 'map',
-    callback: (obj, index) => {
-      ;(obj['2020Year'] = obj['2020Year'] * 1),
-        (obj['2021Year'] = obj['2021Year'] * 1),
-        (obj.Incremental = obj.Incremental * 1)
-      return obj
-    }
+  ).transform({
+    type:'fold',
+    fields:['Exam', 'Enroll'],
+    key: 'type',
+    value: 'value',
   })
-    .transform({
-      type: 'sort-by',
-      fields: ['2021Year'],
-      order: 'DESC'
-    })
-    .transform({
-      type: 'map',
-      callback: (obj, index) => {
-        obj.Index = index + 1
-        return obj
-      }
-    })
-  fs.writeFileSync(EXPORT_FILE_PATH, JSON.stringify(dv.rows), {
+  
+  const data = _.chain(dv.rows).map((item) => {
+    ;(item.Exam = item.Exam * 1), (item.Enroll = +item.Enroll)
+    return item
+  }).value()
+
+  fs.writeFileSync(EXPORT_FILE_PATH, JSON.stringify(data), {
     encoding: 'utf-8',
     flag: 'w'
   })
--- src/utils/FAOData.ts
+++ src/utils/FAOData.ts
@@ -2,7 +2,7 @@
  * @Author: HLGhpz
  * @Date: 2022-05-30 13:30:33
  * @LastEditors: HLGhpz
- * @LastEditTime: 2022-05-31 20:48:47
+ * @LastEditTime: 2022-06-01 11:35:34
  * @Description:
  *
  * Copyright (c) 2022 by HLGhpz, All Rights Reserved.
